e753c6d5109e6e93068530db717620d81452bd99
Jeferson Viana Perito <jefperito@gmail.com>
2016-10-24 22:08:10 -0200

    add more tests

--- mtgz/search_engine.py
+++ mtgz/search_engine.py
@@ -7,11 +7,15 @@ class SearchEngine():
 	def find_by_arguments(self):
 		return self._find_by_arguments
 
+	@find_by_arguments.setter
+	def find_by_arguments(self, arguments):
+		self._find_by_arguments = arguments
+
 	def __init__(self, library={}):
 		pass
 
-	def findBy(self, argument):
-		self._find_by_arguments.append(argument)
+	def find_by(self, argument):
+		self.find_by_arguments.append(argument)
 
 	def filter(self):
 		pass
--- tests/search_engine_test.py
+++ tests/search_engine_test.py
@@ -3,11 +3,22 @@ from mtgz.search_engine import SearchEngine
 
 class TestSearchEngine(unittest.TestCase):
 
-	def test_findBy_name(self):
+	def setUp(self):
+		SearchEngine.find_by_arguments = []
+
+	def test_find_by_with_one_parameters(self):
 		search_engine = SearchEngine()
-		search_engine.findBy('name')
+		search_engine.find_by('name')
 
 		self.assertEqual(['name'], search_engine.find_by_arguments)
+
+	def test_find_by_with_many_parameters(self):
+		search_engine = SearchEngine()
+		search_engine.find_by('name')
+		search_engine.find_by('type')
+		search_engine.find_by('manaCost')
+
+		self.assertEqual(['name', 'type', 'manaCost'], search_engine.find_by_arguments)
 
 if __name__ == '__main__':
 	unittest.main()
